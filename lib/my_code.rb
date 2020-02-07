eec18ff0575156407fffc64b1c4827a4ad1117cb
Johnson Kow <jkow95@gmail.com>
2020-02-07 19:52:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,2 +1,10 @@
 # My Code here....
-def map_to_negative()
+def map_to_negative(source_array)
+  array = []
+  i = 0 
+  while i < source_array.length do 
+    array << source_array[i] * -1
+    i += 1
+  end 
+  array 
+end 
