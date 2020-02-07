88b41d40815e30e277c8f7c2b130610d2a0a25a3
Johnson Kow <jkow95@gmail.com>
2020-02-07 21:00:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -52,3 +52,12 @@ def reduce_to_all_true(source_array)
     i += 1
   end 
 end 
+
+def reduce_to_all_true(source_array)
+  i = 0 
+  while i <source_array.length do 
+    return true if source_array[i]
+    i+= 1 
+  end 
+  
+end 
